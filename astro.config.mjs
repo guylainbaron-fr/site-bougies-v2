a127287b6a36f7edc397816da49e0572e4f0ed7c
guylainbaron-fr <guylain.baron@gmail.com>
2026-01-20 22:22:15 +0100

    Switch to pnpm and Edge adapter to fix Rollup bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/edge"; // BIEN UTILISER /edge ici
 
 export default defineConfig({
   integrations: [tailwind()],
