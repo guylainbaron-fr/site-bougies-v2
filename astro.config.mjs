dcf78d752a289deb7f28b1f03bd3adb78361c386
guylainbaron-fr <guylain.baron@gmail.com>
2026-01-21 00:03:03 +0100

    Keep tailwind config and finalize static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel"; // Import standard
+import vercel from '@astrojs/vercel/serverless'; // On garde l'adaptateur pour les fonctions si besoin
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel({
-    edgeMiddleware: true, // Ceci active le mode Edge sur Vercel
-  }),
+  output: 'static', // ON REPASSE EN STATIQUE
+  adapter: vercel(),
 });
