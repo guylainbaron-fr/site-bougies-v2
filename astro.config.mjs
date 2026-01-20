2c52f202e3f14e7c7101940c2e1c4db98fa398e4
guylainbaron-fr <guylain.baron@gmail.com>
2026-01-21 00:09:34 +0100

    Fix: Remove tsconfig inheritance to bypass missing file error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'; // On garde l'adaptateur pour les fonctions si besoin
-
 export default defineConfig({
-  output: 'static', // ON REPASSE EN STATIQUE
-  adapter: vercel(),
+  output: 'static'
 });
