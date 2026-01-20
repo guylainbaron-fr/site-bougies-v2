3ee84735e7572093c2b3f0d19455446299b0868e
guylainbaron-fr <guylain.baron@gmail.com>
2026-01-20 22:24:33 +0100

    Fix vercel edge import and use pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge"; // BIEN UTILISER /edge ici
+import vercel from "@astrojs/vercel"; // Import standard
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    edgeMiddleware: true, // Ceci active le mode Edge sur Vercel
+  }),
 });
