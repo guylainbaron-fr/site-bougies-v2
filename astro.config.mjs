0028dc078774c3de965597f6496febc85380f57f
guylainbaron-fr <guylain.baron@gmail.com>
2026-09-17 10:55:21 +0200

    fix(seo): mise a jour robots.txt et generation automatique sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,28 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'server', // On active le mode serveur
+  site: 'https://jf-aniuta.vercel.app',
+  output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: [
+        'https://jf-aniuta.vercel.app/boutique?cat=Sculptures',
+        'https://jf-aniuta.vercel.app/boutique?cat=Tableaux',
+        'https://jf-aniuta.vercel.app/boutique?cat=Accessoires',
+        'https://jf-aniuta.vercel.app/boutique?cat=Tous',
+      ],
+      filter: (page) =>
+        !page.includes('/api/') &&
+        !page.includes('/panier') &&
+        !page.includes('/success') &&
+        !page.includes('/cancel') &&
+        !page.includes('/merci') &&
+        !page.includes('/au-revoir'),
+    }),
+  ],
 });
